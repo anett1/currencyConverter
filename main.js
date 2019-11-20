343d2bb8301e1534b8dbe6b993aac32e7a53047c
anett1 <48224655+anett1@users.noreply.github.com>
2019-11-20 19:00:09 +0100

    address API change

--- main.js
+++ main.js
@@ -1,77 +1,81 @@
-const table = document.querySelector(".exchangeRate__table tbody");
-const dateSpan = document.querySelector(".exchangeRate h1 span");
-const myCurrency = document.querySelector("[name=MyCurrency]");
-const wantCurrency = document.querySelector("[name=WantCurrency]");
-const valueCurrency = document.querySelector(".formCurrencyConverter__value");
-const btnCurrency = document.querySelector(".formCurrencyConverter__btn");
-const convertedCurrency = document.querySelector(
-  ".formCurrencyConverter__result"
-);
+document.addEventListener("DOMContentLoaded", () => {
 
-let date = new Date();
-let dateForRate = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
+  const table = document.querySelector(".exchangeRate__table tbody");
+  const dateSpan = document.querySelector(".exchangeRate h1 span");
+  const myCurrency = document.querySelector("[name=MyCurrency]");
+  const wantCurrency = document.querySelector("[name=WantCurrency]");
+  const valueCurrency = document.querySelector(".formCurrencyConverter__value");
+  const btnCurrency = document.querySelector(".formCurrencyConverter__btn");
+  const convertedCurrency = document.querySelector(
+    ".formCurrencyConverter__result"
+  );
 
-function getExchangeRate() {
-  return fetch(`http://api.nbp.pl/api/exchangerates/tables/a/${dateForRate}/`)
-    .then(resp => resp.json())
-    .then(currencyList => {
-      return currencyList[0].rates;
-    });
-}
-getExchangeRate().then(currencyList => {
-  dateSpan.textContent = dateForRate;
+  let dateCurrent = new Date();
+  let dateCurrentFormat = `${dateCurrent.getFullYear()}-${dateCurrent.getMonth()}-${dateCurrent.getDate()}`;
 
-  currencyList.forEach(currency => {
-    let rowTable = document.createElement("tr");
-    rowTable.innerHTML = `<td>${currency.code}</td><td>${
+  function getExchangeRate() {
+    return fetch(`http://api.nbp.pl/api/exchangerates/tables/a/`)
+      .then(resp => resp.json())
+      .then(currencyList => {
+        return currencyList[0].rates;
+      });
+  }
+  getExchangeRate().then(currencyList => {
+    dateSpan.textContent = dateCurrentFormat;
+
+    currencyList.forEach(currency => {
+      let rowTable = document.createElement("tr");
+      rowTable.innerHTML = `<td>${currency.code}</td><td>${
       currency.currency
     }</td><td>${currency.mid.toFixed(4)}</td>`;
-    table.appendChild(rowTable);
+      table.appendChild(rowTable);
+    });
+    currencyList.forEach(currency => {
+      let selectOptionCurrency = document.createElement("option");
+      selectOptionCurrency.setAttribute("value", `${currency.mid.toFixed(4)}`);
+      selectOptionCurrency.setAttribute("data-code", `${currency.code}`);
+      selectOptionCurrency.innerHTML = `${currency.code} ${currency.currency}`;
+      myCurrency.appendChild(selectOptionCurrency);
+    });
+    currencyList.forEach(currency => {
+      let selectOptionCurrency = document.createElement("option");
+      selectOptionCurrency.setAttribute("value", `${currency.mid.toFixed(4)}`);
+      selectOptionCurrency.setAttribute("data-code", `${currency.code}`);
+      selectOptionCurrency.innerHTML = `${currency.code} ${currency.currency}`;
+      wantCurrency.appendChild(selectOptionCurrency);
+    });
+
   });
-  currencyList.forEach(currency => {
-    let selectOptionCurrency = document.createElement("option");
-    selectOptionCurrency.setAttribute("value", `${currency.mid.toFixed(4)}`);
-    selectOptionCurrency.setAttribute("data-code", `${currency.code}`);
-    selectOptionCurrency.innerHTML = `${currency.code} ${currency.currency}`;
-    myCurrency.appendChild(selectOptionCurrency);
+
+  btnCurrency.addEventListener("click", e => {
+    e.preventDefault();
+    let valueCurrencyNumber = valueCurrency.value;
+
+    let myCurrencySelectIndex = document.querySelector("[name=MyCurrency]")
+      .selectedIndex;
+    let myCurrencySelectOptions = document.querySelector("[name=MyCurrency]")
+      .options;
+    let myCurrencySelectValue =
+      myCurrencySelectOptions[myCurrencySelectIndex].value;
+    let myCurrencySelectData = myCurrencySelectOptions[
+      myCurrencySelectIndex
+    ].getAttribute("data-code");
+
+    let wantCurrencySelectIndex = document.querySelector("[name=WantCurrency]")
+      .selectedIndex;
+    let wantCurrencySelectOptions = document.querySelector("[name=WantCurrency]")
+      .options;
+    let wantCurrencySelectValue =
+      wantCurrencySelectOptions[wantCurrencySelectIndex].value;
+    let wantCurrencySelectData = wantCurrencySelectOptions[
+      wantCurrencySelectIndex
+    ].getAttribute("data-code");
+
+    let convertedValue = (
+      (parseInt(valueCurrencyNumber) * parseInt(myCurrencySelectValue)) /
+      parseInt(wantCurrencySelectValue)
+    ).toFixed(2);
+
+    convertedCurrency.textContent = `${valueCurrencyNumber} ${myCurrencySelectData} = ${convertedValue} ${wantCurrencySelectData}`;
   });
-  currencyList.forEach(currency => {
-    let selectOptionCurrency = document.createElement("option");
-    selectOptionCurrency.setAttribute("value", `${currency.mid.toFixed(4)}`);
-    selectOptionCurrency.setAttribute("data-code", `${currency.code}`);
-    selectOptionCurrency.innerHTML = `${currency.code} ${currency.currency}`;
-    wantCurrency.appendChild(selectOptionCurrency);
-  });
-});
-
-btnCurrency.addEventListener("click", e => {
-  e.preventDefault();
-  let valueCurrencyNumber = valueCurrency.value;
-
-  let myCurrencySelectIndex = document.querySelector("[name=MyCurrency]")
-    .selectedIndex;
-  let myCurrencySelectOptions = document.querySelector("[name=MyCurrency]")
-    .options;
-  let myCurrencySelectValue =
-    myCurrencySelectOptions[myCurrencySelectIndex].value;
-  let myCurrencySelectData = myCurrencySelectOptions[
-    myCurrencySelectIndex
-  ].getAttribute("data-code");
-
-  let wantCurrencySelectIndex = document.querySelector("[name=WantCurrency]")
-    .selectedIndex;
-  let wantCurrencySelectOptions = document.querySelector("[name=WantCurrency]")
-    .options;
-  let wantCurrencySelectValue =
-    wantCurrencySelectOptions[wantCurrencySelectIndex].value;
-  let wantCurrencySelectData = wantCurrencySelectOptions[
-    wantCurrencySelectIndex
-  ].getAttribute("data-code");
-
-  let convertedValue = (
-    (parseInt(valueCurrencyNumber) * parseInt(myCurrencySelectValue)) /
-    parseInt(wantCurrencySelectValue)
-  ).toFixed(2);
-
-  convertedCurrency.textContent = `${valueCurrencyNumber} ${myCurrencySelectData} = ${convertedValue} ${wantCurrencySelectData}`;
 });
